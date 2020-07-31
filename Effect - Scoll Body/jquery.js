b94569e211faf25e91ba2689057cb8072bea12c1
Vo Tung Lam <lam.vo@banvien.com>
2020-07-31 18:49:12 +0700

    Scroll

--- Effect - Scoll Body/jquery.js
+++ Effect - Scoll Body/jquery.js
@@ -1,12 +1,38 @@
 $( document ).ready(function() {
-
-    function scrollToElement(){
-      $('html').animate({scrollTop: 1900}, 1000);
-    };
-
-    $('ul.menu li.item').click(function() {
-      scrollToElement();
-      console.log('I am here.')
+  var n = 6;
+    $('html').animate({scrollTop: 0}, 2000);
+    $('ul.menu li.item:nth-child(1)').click(function() {
+      $('html').animate({scrollTop: $('div.block:nth-child(2)').offset().top}, 1000);
+      $('div').removeClass('selected');
+      $(this).addClass('selected')
+      });
+    $('ul.menu li.item:nth-child(2)').click(function() {
+      $('html').animate({scrollTop: $('div.block:nth-child(3)').offset().top}, 1000);
+      $('div.block').removeClass('selected');
+      $(this).addClass('selected')
+      });
+    $('ul.menu li.item:nth-child(3)').click(function() {
+      $('html').animate({scrollTop: $('div.block:nth-child(4)').offset().top}, 1000);
+      $('div.block').removeClass('selected');
+      $(this).addClass('selected')
+      });
+    $('ul.menu li.item:nth-child(4)').click(function() {
+      $('html').animate({scrollTop: $('div.block:nth-child(5)').offset().top}, 1000);
+      $('div.block').removeClass('selected');
+      $(this).addClass('selected')
+      });
+    $('ul.menu li.item:nth-child(5)').click(function() {
+      $('html').animate({scrollTop: $('div.block:nth-child(6)').offset().top}, 1000);
+      $('div.block').removeClass('selected');
+      $(this).addClass('selected')
+      });
+    $('ul.menu li.item:nth-child(6)').click(function() {
+        $('html').animate({scrollTop: $('div.block:nth-child(7)').offset().top}, 1000);
+        $('div.block').removeClass('selected');
+        $(this).addClass('selected')
+        });
+    $('.backToTop').click(function() {
+      $('html').animate({scrollTop: 0}, 2000);
       });
     
 });
