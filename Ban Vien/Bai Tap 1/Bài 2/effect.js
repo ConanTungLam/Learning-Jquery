c50a6d14f8360b7d0fd7d1c38196e4f000e15a06
Vo Tung Lam <lam.vo@banvien.com>
2020-08-25 10:11:11 +0700

    1 vài lưu ý

--- Ban Vien/Bai Tap 1/Bài 2/effect.js
+++ Ban Vien/Bai Tap 1/Bài 2/effect.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
 
-    $('.close-pop-up').click(function (e){
-        //do something
-    });
+ 
+
+
     $('.infor-stu .show-detail-stu').click(function (e) { 
         $('.infor-stu .show-detail-stu').empty().append('Show Delail');
         $(this).append('<div class="pop-up-infor"><span class="arrow"></span><div class="pop-up-name">Nguyễn Văn A  <div class="close-pop-up">x</div></div><div class="pop-up-person-infor"> Địa chỉ<br/> Ngày sinh<br/> Số ĐT<br/> Email<br/></div><div class="pop-up-score"> Điểm môn 1<br/> Điểm môn 2<br/> Điểm môn 3<br/> Điểm môn 4<br/> Điểm môn 5<br/> Điểm môn 6<br/> Điểm môn 7<br/></div><div class="pop-up-rank"> Điểm trung bình<br> Xếp loại: Giỏi/Khá/Trung Bình</div></div>');
-        popUpPosition(this)
+        popUpPosition(this);
+
+        $('.close-pop-up').click(function (e){
+            e.stopPropagation();
+            $(this).closest('.pop-up-infor').css("color", "blue");
+        }); 
     });
 
+  
+
     function popUpPosition(element){
-        console.log($(this).parent('.infor-stu'));
-        console.log($('.pop-up-infor').outerWidth());
+        console.log($(element).parent('.infor-stu').offset().left);
+        //console.log($('.pop-up-infor').outerWidth());
         //overlap góc dưới bên phải & dòng dưới cùng
         if($('.pop-up-infor').offset().top + $('.pop-up-infor').outerHeight() > $('.show-stu').outerHeight(true) 
         && $('.infor-stu').offset().left > $('.pop-up-infor').outerWidth()){
@@ -32,5 +39,32 @@ $(document).ready(function () {
         //}
 
     }
+
+
+    $('.selector').each(function(index, element){
+        console.log(index, element);
+    });
+
+
+    var callBackFunc = function(index, element){
+        console.log(idx, item);
+    }
+    
+
+
+    
+    forEachElement('selector', callBackFunc);
+
+    function forEachElement(selector, callBackFunc){
+        var listElement = document.getElementsByClassName(selector);
+
+        for(var i = 0; i < listElement.length; i++){
+            callBackFunc(i, listElement[i]);
+        }
+
+        console.log(i);
+
+    }
+    
         
 });
