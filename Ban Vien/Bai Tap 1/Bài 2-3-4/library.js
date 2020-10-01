8aa65fcbbd2101358d01abb0670bf01b6cc290e4
Your Name <lamvo@hoanghacgroup.com>
2020-10-02 01:15:24 +0700

    show list studend - popup + position pop-up

--- Ban Vien/Bai Tap 1/Bài 2-3-4/library.js
+++ Ban Vien/Bai Tap 1/Bài 2-3-4/library.js
@@ -2,7 +2,7 @@ function StudentGroup() {
     var EDIT_MODAL = null;
 
     // private properties
-   
+
 
 
     this.renderLayout = function (listStudentInfo) {
@@ -10,7 +10,6 @@ function StudentGroup() {
 
         for (var i = 0; i < listStudentInfo.length; i++) {
             _generateStudentInfo($container, listStudentInfo[i]);
-            console.log("Hello");
         }
 
         return $container;
@@ -24,7 +23,6 @@ function StudentGroup() {
 
         // goi tiep ham generate de student info tao ra modal cua rieng no, sau do append vao container
         var studentModal = studentInfo.renderLayout();
-        console.log(studentModal);
         $container.append(studentModal);
 
 
@@ -92,7 +90,7 @@ function StudentInfo(studentData) {
     var address = studentData.address;
     var email = studentData.email;
     var score = studentData.score;
-    //var COMPONENT = null;
+    var COMPONENT = null;
     var COMPONENT_DETAIL = null;
 
     var me = this; // tai thoi diem nay su dung variable me thay the cho this. de viec goi cac function de dang hon, xem o _bindEvent o duoi, me.editStudent se trigger cai event o tren.
@@ -100,26 +98,13 @@ function StudentInfo(studentData) {
     // render layout se nhan cac param tuy theo minh quyet dinh, minh co the gui lai 1 Object hoac minh cung co the dua container ngoai kia vao day de append no. Trong vi du nay, ta su dung 1 container cha de sinh ra cai nay,
     // vi vay ta can phai tra ve component modal (COMPONENT o day la output, va input la null);
 
-    logData(studentData);
-
-
-
-    function logData(data){
-        console.log(data)
-    }
-
-
     this.renderLayout = function () {
-        //COMPONENT = _generateLayout();
-        var studentBlockEl = $('<div class="infor-stu" id="' + id + '"><div class="name-stu"> ' + name + ' <span class="edit-stu">&#9998;</span><span class="delete-stu">&#10008;</span></div> <div class="avatar-stu">avatar</div> <div class="more-info">Ngay sinh: ' + birthday + '<br>Số ĐT: ' + phone + '<br> Email: ' + email + ' </div> <div class="show-detail-stu">Show Delail</div></div>');
-
-         //_bindEvent();
-
-        return studentBlockEl;
-        //return COMPONENT;
+        COMPONENT = _generateLayout();
+        _bindEvent();
+        return COMPONENT;
     };
 
-    
+
 
     //  this.renderAndAppendLayout = function ($container) {
     // //     COMPONENT = _generateLayout();
@@ -130,17 +115,37 @@ function StudentInfo(studentData) {
     // // };
 
     this.showStudentDetail = function () {
+
         if (COMPONENT_DETAIL == null) {
+            $('.pop-up-infor').hide();
             COMPONENT_DETAIL = _generateComponentDetail();
+            showUserDetailModal()
         }
 
-        _showUserDetailModal();
-
-        function _showUserDetailModal() {
-
-            COMPONENT_DETAIL.addClass("active");
+        function showUserDetailModal() {
+            // COMPONENT_DETAIL.addClass("active");
             // add adjust position for component here.
+            //overlap dòng dưới bên trái
+
+            if (COMPONENT_DETAIL.offset().top + COMPONENT_DETAIL.outerHeight() > $('.show-stu').offset().top + $('.show-stu').outerHeight()
+                && $('.show-stu').outerWidth() - COMPONENT_DETAIL.offset().left > COMPONENT_DETAIL.outerWidth()) {
+                COMPONENT_DETAIL.css({ "top": -COMPONENT_DETAIL.outerHeight(true) + 35 + "px", "right": -COMPONENT_DETAIL.outerWidth(true) + 20 + "px" });
+                COMPONENT_DETAIL.find('.arrow').removeClass().addClass('arrow bottom-left')
+            }
+
+            //overlap góc dưới bên phải & dòng dưới cùng & dòng trên cùng góc phải
+            else if ((COMPONENT_DETAIL.offset().top + COMPONENT_DETAIL.outerHeight() > $('.show-stu').offset().top + $('.show-stu').outerHeight()
+                && COMPONENT_DETAIL.offset().right < COMPONENT_DETAIL.outerWidth()) ||
+                COMPONENT_DETAIL.offset().left + COMPONENT_DETAIL.outerWidth() > $('.show-stu').offset().left + $('.show-stu').outerWidth()
+                && COMPONENT_DETAIL.offset().left > COMPONENT_DETAIL.outerWidth()) {
+                COMPONENT_DETAIL.css({ "top": -COMPONENT_DETAIL.outerHeight(true) + 35 + "px", "left": -COMPONENT_DETAIL.outerWidth(true) + 20 + "px" });
+                COMPONENT_DETAIL.find('.arrow').removeClass().addClass('arrow bottom-right')
+            }
+            else {
+                COMPONENT_DETAIL.find('.arrow').removeClass().addClass('arrow top-left')
+            }
         }
+
     };
 
     this.editStudent = function () {
@@ -184,16 +189,17 @@ function StudentInfo(studentData) {
 
     // add your code here
     function _generateLayout() {
-       
+        var studentBlockEl = $('<div class="infor-stu" id="' + id + '"><div class="name-stu"> ' + name + ' <span class="edit-stu">&#9998;</span><span class="delete-stu">&#10008;</span></div> <div class="avatar-stu">avatar</div> <div class="more-info">Ngày sinh: ' + birthday + '<br>Số ĐT: ' + phone + '<br> Email: ' + email + ' </div> <div class="show-detail-stu">Show Delail</div></div>');
+        return studentBlockEl;
     }
 
     function _bindEvent() {
-        COMPONENT.find(".show-user-detail-button").click(function () {
+        COMPONENT.find(".show-detail-stu").click(function () {
             me.showStudentDetail();
         });
 
         COMPONENT.find(".edit-button").click(function () {
-            me.editStudent();
+            editStudent();
         });
     }
 
@@ -201,7 +207,22 @@ function StudentInfo(studentData) {
     // add your code here
     function _generateComponentDetail() {
 
+        var modal = $('<div class="pop-up-infor" id=' + id + '><span class="arrow"></span><div class="pop-up-name">' + name + '<div class="close-pop-up">x</div></div><div class="pop-up-person-infor"> Địa chỉ: ' + address + '<br/> Ngày sinh: ' + birthday + '<br/> Số ĐT: ' + phone + '<br/> Email: ' + email + '<br/></div><div class="pop-up-score"> Điểm môn 1: ' + score[0] + '<br/>  Điểm môn 2: ' + score[1] + '<br/> Điểm môn 3: ' + score[2] + '<br/> Điểm môn 4: ' + score[3] + '<br/> Điểm môn 5: ' + score[4] + '<br/> Điểm môn 6: ' + score[5] + '<br/> Điểm môn 7: ' + score[6] + '<br/></div><div class="pop-up-rank"> Điểm trung bình: <br> Xếp loại: Giỏi/Khá/Trung Bình</div></div>');
+        COMPONENT.find('.show-detail-stu').append(modal);
+        return modal;
+
         // return block content
+    }
+
+    function closePopUp(OneElementClose) {
+        $(OneElementClose).removeClass('active');
+        $(OneElementClose).children('.pop-up-infor').css({ "opacity": "0", "transform": "scale(0.3)", "zIndex": "-1" });
+    }
+
+    function openPopUp(OneElementOpen) {
+        $('.show-detail-stu').removeClass('active').children('.pop-up-infor').css({ "opacity": "0", "transform": "scale(0.3)", "zIndex": "-1" });
+        $(OneElementOpen).addClass('active');
+        $(OneElementOpen).children('.pop-up-infor').css({ "opacity": "1", "transform": "scale(1)", "zIndex": "3" });
     }
 
 
@@ -216,7 +237,7 @@ $(document).ready(function () {
         phone: "0323455547",
         address: "12 Tôn Đản",
         email: 'infoa@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [7, 5, 10, 8, 5, 6, 7]
     },
     {
         id: 2,
@@ -226,7 +247,7 @@ $(document).ready(function () {
         phone: "0323467547",
         address: "12 Tôn Đản",
         email: 'infob@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [5, 5, 3, 4, 5, 6, 5]
     },
     {
         id: 3,
@@ -236,7 +257,7 @@ $(document).ready(function () {
         phone: "0356467547",
         address: "12 Tôn Đản",
         email: 'infoc@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [3, 2, 3, 7, 5, 8, 7]
     },
     {
         id: 4,
@@ -246,7 +267,7 @@ $(document).ready(function () {
         phone: "0323466547",
         address: "12 Tôn Đản",
         email: 'infod@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [2, 2, 3, 2, 3, 4, 3]
     },
     {
         id: 5,
@@ -256,7 +277,7 @@ $(document).ready(function () {
         phone: "0323667547",
         address: "12 Tôn Đản",
         email: 'infoe@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [8, 8, 9, 8, 10, 9, 9]
     },
     {
         id: 6,
@@ -266,7 +287,7 @@ $(document).ready(function () {
         phone: "0323889547",
         address: "12 Tôn Đản",
         email: 'infof@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [7, 7, 8, 6, 8, 6, 9]
     },
     {
         id: 7,
@@ -276,7 +297,7 @@ $(document).ready(function () {
         phone: "0323985476",
         address: "12 Tôn Đản",
         email: 'infog@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [10, 9, 9, 8, 10, 10, 8]
     },
     {
         id: 8,
@@ -286,7 +307,7 @@ $(document).ready(function () {
         phone: "0323800047",
         address: "12 Tôn Đản",
         email: 'infoh@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [7, 5, 5, 5, 5, 6, 7]
     },
     {
         id: 9,
@@ -296,7 +317,7 @@ $(document).ready(function () {
         phone: "0323889547",
         address: "12 Tôn Đản",
         email: 'infoi@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [4, 8, 9, 9, 7, 6, 8]
     },
     {
         id: 10,
@@ -306,9 +327,10 @@ $(document).ready(function () {
         phone: "0323223547",
         address: "12 Tôn Đản",
         email: 'infoj@gmail.com',
-        score: [1, 2, 3, 4, 5, 6, 7]
+        score: [10, 8, 5, 6, 9, 6, 9]
     }];
 
     var container = STUDENT_GROUP_UTILS.renderLayout(listStudent);
     $('.show-stu').append(container);
+
 });
